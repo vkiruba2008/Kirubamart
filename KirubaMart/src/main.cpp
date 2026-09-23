ca4b289933a98ef66fc2dd956bf8dfebe8c20756
YOUR NAME <YOUR_GITHUB_EMAIL>
2026-09-23 10:52:42 +0530

    Updated KirubaMart frontend and cart

--- KirubaMart/src/main.cpp
+++ KirubaMart/src/main.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
 #include <drogon/drogon.h>
-
+#include <drogon/orm/DbClient.h>
 int main()
 {
+    auto dbClient = drogon::orm::DbClient::newPgClient(
+   "host=localhost port=5432 dbname=kirubamart user=postgres password=YOUR_PASSWORD",
+    1
+);
+
+std::cout << "Database client created successfully!" << std::endl;
     drogon::app().registerHandler(
         "/",
         [](const drogon::HttpRequestPtr&,
